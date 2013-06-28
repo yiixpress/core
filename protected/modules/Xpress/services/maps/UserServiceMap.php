520f7d386cb2e81ddb8fa8df740d506f879206ec
Hung Nguyen <hung.nguyen@webflexica.com>
2013-06-28 14:54:40 +0700

    User services

---
+++ protected/modules/Xpress/services/maps/UserServiceMap.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'UserApi' => array(
+		'class'   => 'Xpress.services.UserApi',
+		'methods' => array(
+			'login' => array(
+				'title' => 'Authorize and login user',
+			),
+		),
+	),
+);
+?>
